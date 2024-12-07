b150a63f5fd22542b27444281011e6e208750073
Koen Rademaker <k.rademaker97@protonmail.com>
2024-12-07 12:12:05 +0100

    Added second challenge

---
+++ 2024/02.py
@@ -0,0 +1,45 @@
+def is_safe_report(report: list[int]) -> int | None:
+    is_decreasing = True
+    is_increasing = True
+    for index, level in enumerate(report):
+        has_next_level = index < len(report) - 1
+
+        if not has_next_level:
+            continue
+
+        next_level = report[index + 1]
+
+        is_decreasing = level > next_level
+
+        if is_decreasing == is_increasing and index > 0:
+            return False
+
+        is_increasing = level < next_level
+
+        if not is_decreasing and not is_increasing or is_decreasing and is_increasing:
+            return False
+
+        difference = level - next_level if is_decreasing else next_level - level
+        if difference > 3:
+            return False
+    
+    return True
+
+with open("input/02.txt") as file:
+    reports = []
+    for file in file.readlines():
+        report = [int(level) for level in file.split()]
+        reports.append(report)
+
+total_safe_reports = 0
+for report in reports:
+    if is_safe_report(report):
+        total_safe_reports += 1
+        continue
+
+    for index in range(len(report)):
+        new_report = list(report)
+        new_report.pop(index)
+        if is_safe_report(new_report):
+            total_safe_reports += 1
+            break
