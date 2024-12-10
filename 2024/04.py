0e2a17ffcf2b713c652fbfb5afc8315eae31a88b
Koen Rademaker <k.rademaker97@protonmail.com>
2024-12-11 00:10:10 +0100

    Added part 2 of day 4

--- 2024/04.py
+++ 2024/04.py
@@ -1,3 +1,5 @@
+is_part_one = True
+
 with open("input/04.txt") as file:
     data = []
     for line in file.readlines():   
@@ -6,12 +8,14 @@ with open("input/04.txt") as file:
     grid_width = len(data[0])
     grid_height = len(data)
 
-search_word = 'XMAS'
+search_word = 'XMAS' if is_part_one else 'SAM'
 first_letter = search_word[0]
 search_word_lenght = len(search_word)
 word_range = range(search_word_lenght)
 
-possible_words = []
+number_of_matches = 0
+number_of_x_patterns = 0
+indexes_of_middle_character = []
 
 for y, row in enumerate(data):
     for x, character in enumerate(row):
@@ -31,36 +35,64 @@ for y, row in enumerate(data):
 
         if is_possible_vertical_up:
             word = ''.join([data[y - index][x] for index in word_range])
-            possible_words.append(word)
-
+            if word == search_word:
+                number_of_matches += 1
         if is_possible_vertical_down:
             word = ''.join([data[y + index][x] for index in word_range])
-            possible_words.append(word)
-        
+            if word == search_word:
+                number_of_matches += 1        
         if is_possible_horizontal_forward:
             word = ''.join([data[y][x + index] for index in word_range])
-            possible_words.append(word)
-        
+            if word == search_word:
+                number_of_matches += 1        
         if is_possible_horizontal_backward:
             word = ''.join([data[y][x - index] for index in word_range])
-            possible_words.append(word)
-
+            if word == search_word:
+                number_of_matches += 1
         if is_possible_diagonal_right_up:
             word = ''.join([data[y - index][x + index] for index in word_range])
-            possible_words.append(word)
+
+            if word == search_word:
+                number_of_matches += 1
+
+                middle_character_index = (y - 1, x + 1)
+                if middle_character_index not in indexes_of_middle_character:
+                    indexes_of_middle_character.append(middle_character_index)
+                else:
+                    number_of_x_patterns += 1
 
 
         if is_possible_diagonal_left_up:
             word = ''.join([data[y - index][x - index] for index in word_range])
-            possible_words.append(word)
+            if word == search_word:
+                number_of_matches += 1
+            
+                middle_character_index = (y - 1, x - 1)
+                if middle_character_index not in indexes_of_middle_character:
+                    indexes_of_middle_character.append(middle_character_index)
+                else:
+                    number_of_x_patterns += 1
+
 
         if is_possible_diagonal_right_down:
             word = ''.join([data[y + index][x + index] for index in word_range])
-            possible_words.append(word)
+            if word == search_word:
+                number_of_matches += 1
+            
+                middle_character_index = (y + 1, x + 1)
+                if middle_character_index not in indexes_of_middle_character:
+                    indexes_of_middle_character.append(middle_character_index)
+                else:
+                    number_of_x_patterns += 1
+
 
         if is_possible_diagonal_left_down:
             word = ''.join([data[y + index][x - index] for index in word_range])
-            possible_words.append(word)
-
-
-total = possible_words.count(search_word)
+            if word == search_word:
+                number_of_matches += 1
+            
+                middle_character_index = (y + 1, x - 1)
+                if middle_character_index not in indexes_of_middle_character:
+                    indexes_of_middle_character.append(middle_character_index)
+                else:
+                    number_of_x_patterns += 1
